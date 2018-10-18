309ddc8d2946113e0b82cce98456028b688c56d2
Matt Etress <mattetress@me.com>
2018-10-18 00:18:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,6 +42,17 @@ class Student
     DB[:conn].execute(sql).map { |row| Student.new_from_db(row) }
   end
 
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 10
+    SQL
+
+    DB[:conn].execute(sql).map { |row| Student.new_from_db(row) }
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
