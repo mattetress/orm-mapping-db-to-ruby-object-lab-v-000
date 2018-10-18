cb9982a101510b86d9c85f2247a8ee3e08742af0
Matt Etress <mattetress@me.com>
2018-10-18 00:24:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,12 +32,23 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map { |row| Student.new_from_db(row) }
+  end
+
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade < 12
     SQL
+    
     DB[:conn].execute(sql).map { |row| Student.new_from_db(row) }
   end
 
